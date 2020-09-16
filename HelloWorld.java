611aaece94a103b8c616a3ae06cc990cdc3fd6cb
Rosikrusian <hifdzi.hisan.alamin@gmail.com>
2020-09-16 14:51:21 +0700

    some init msg

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        int a=10;
+        int b=20;
+        int c=a * b;
+        System.out.println("Hello World!");
+        System.out.println("Nama = Hifdzi Hisan");
+        System.out.println("Nama Panggilan = Hifdzi");
+        System.out.println("Nim = 6706204112");
+        System.out.println("Kelas = D3IF4401");
+        System.out.println("10 * 20 = "+ c);
+
+    }
+}
